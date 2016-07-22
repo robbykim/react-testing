5fa5a9c45b387be28f4cfe662124140909ee5e7d
Alex Bang (MBP) <alexbang@hotmail.com>
2016-07-22 12:48:50 -0400

    Pass correct number of images

--- js/gallery.jsx
+++ js/gallery.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var Image = require('./image');
 
 var Gallery = function (props) {
-  console.log(props);
+  //console.log(props);
   var images = props.images.map(function(image, index) {
     return <Image url={image.url} description={image.description} key={index}/>;
   });
--- test/test-gallery.jsx
+++ test/test-gallery.jsx
@@ -13,7 +13,14 @@ describe('Gallery component', function() {
   })
 
   it('Should have the correct number of Images rendered', function() {
-
+    var image1 = {
+      url: "www.example.com/image1",
+      description: "image1"
+    }
+    renderer.render(<Gallery images={[image1]} />)
+    var result = renderer.getRenderOutput();
+    console.log(result.props.children.length, "children.length");
+    // result.props.should.equal('gallery')
   })
 
   it('Should have the correct props set for each image', function() {
